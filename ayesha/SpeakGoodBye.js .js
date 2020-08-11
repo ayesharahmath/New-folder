94395fcea7585652ca044a0431cb4949c4187c34
Ayesha <ayesharahmath34@gmail.com>
2020-08-11 12:13:03 +0530

    module 4

---
+++ ayesha/SpeakGoodBye.js .js
@@ -0,0 +1,8 @@
+﻿( function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.speakBye ="Good Bye";
+  byeSpeaker.speak = function(name) {
+  console.log(byeSpeaker.speakBye + " " + name);
+}
+ window.byeSpeaker = byeSpeaker;
+}) (window);
